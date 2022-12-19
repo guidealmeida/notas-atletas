a44b004fcc5e2b630b53f9322d3c524437fa3de8
unknown <ghp-almeida@hotmail.com>
2022-12-19 19:52:47 -0300

    segundo commit

--- dados-atletas.js
+++ dados-atletas.js
@@ -1,78 +1,76 @@
 class Atleta {
-    constructor(nome, idade, peso, altura, anotacoes) {
-        this.nomeAtleta = nome;
-        this.idadeAtleta = idade;
-        this.pesoAtleta = peso;
-        this.alturaAtleta = altura;
-        this.notasAtleta = anotacoes;
+    constructor(nome, idade, peso, altura, notas) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+        this.notas = notas
     }
+
     calculaCategoria() {
-        if(this.idadeAtleta >= 9 && this.idadeAtleta <= 11){
-            this.categoriaAtleta = "Infantil";
-        }
-        else if(this.idadeAtleta === 12 || this.idadeAtleta === 13) {
-            this.categoriaAtleta = "Juvenil";
-        }
-        else if(this.idadeAtleta === 14 || this.idadeAtleta === 15) {
-            this.categoriaAtleta = "Intermediário";
-        }
-        else if(this.idadeAtleta >= 16 && this.idadeAtleta <= 30) {
-            this.categoriaAtleta = "Adulto";
-        }
-        else {
-            this.categoriaAtleta = "Sem categoria";
-        }
+        if (this.idade >= 9 && this.idade <= 11)
+            return "Infantil"
+        if (this.idade >= 12 && this.idade <= 13)
+            return "Juvenil"
+        if (this.idade >= 14 && this.idade <= 15)
+            return "Intermediário"
+        if (this.idade >= 16 && this.idade <= 30)
+            return "Adulto"
     }
+
     calculaIMC() {
-        this.atletaIMC = this.pesoAtleta/(this.alturaAtleta*this.alturaAtleta);
+        return this.peso / (this.altura * this.altura)
     }
+
     calculaMediaValida() {
-        let notasOrdenadas = this.notasAtleta.sort((b, a) => a - b);
-        let notasValidas = notasOrdenadas.slice(1,4);
-        let somaNotasValidas = notasValidas.reduce(function(total, atual){
-            return total+=atual;
-        }, 0);
-        this.mediaValida = somaNotasValidas/notasValidas.length;
+        let soma = 0
+        let media = 0
+        let notas = this.notas.sort()
+        let notasComputadas = notas.slice(1,4)
+        
+        notasComputadas.forEach(function(nota) {
+            soma = soma + nota
+        })
+        media = soma / notasComputadas.length
+        return media
     }
-    obtemNomeAtleta() {
-        console.log(`Nome: ${this.nomeAtleta}`);
+
+    obtemNomeAtleta(){
+        return this.nome
     }
+
     obtemIdadeAtleta() {
-        console.log(`Idade: ${this.idadeAtleta}`);
+        return this.idade;
     }
+
     obtemPesoAtleta() {
-        console.log(`Peso: ${this.pesoAtleta}`);
+        return this.peso;
     }
-    obtemAlturaAtleta() {
-        console.log(`Altura: ${this.alturaAtleta}`);
-    }
+
     obtemNotasAtleta() {
-        console.log(`Notas: ${this.notasAtleta}`);
-    }
-    obtemCategoria() {
-        console.log(`Categoria: ${this.categoriaAtleta}`);
-    }
-    obtemIMC() {
-        console.log(`IMC: ${this.atletaIMC}`);
-    }
-    obtemMediaValida() {
-        console.log(`Média Válida: ${this.mediaValida}`);
-        console.log("-----------");
-    }
+        return this.notas;
+    } 
 }
 
+// Declara o atleta
 const atleta = new Atleta("Cesar Abascal",
     30, 80, 1.70,
     [10, 9.34, 8.42, 10, 7.88]);
 
-atleta.obtemNomeAtleta();
-atleta.obtemIdadeAtleta();
-atleta.obtemPesoAtleta();
-atleta.obtemAlturaAtleta();
-atleta.obtemNotasAtleta();
-atleta.calculaCategoria();
-atleta.obtemCategoria();
-atleta.calculaIMC();
-atleta.obtemIMC();
-atleta.calculaMediaValida();
-atleta.obtemMediaValida();
+console.log("Nome: " + atleta.obtemNomeAtleta());
+console.log("Idade: " + atleta.obtemIdadeAtleta());
+console.log("Peso: " + atleta.obtemPesoAtleta());
+console.log("Notas: " + atleta.obtemNotasAtleta());
+console.log("Categoria: " + atleta.calculaCategoria());
+console.log("IMC: " + atleta.calculaIMC());
+console.log("Média válida: " + atleta.calculaMediaValida());
+
+// Exemplo de Saída
+// Nome: Cesar Abascal
+// Idade: 30
+// Peso: 80
+// Altura: 1.7
+// Notas: 10,9.34,8.42,10,7.88
+// Categoria: Adulto
+// IMC: 27.68166089965398
+// Média válida: 8.766666666666666
